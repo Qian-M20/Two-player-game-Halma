731794b55b5b4aeec2a3c28641e11e05c03cdf5c
Qian <qian.ma3@dcmail.ca>
2020-02-26 11:08:21 -0500

    Basic skeleton set up

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './App.css';
+// import logo from './logo.svg'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="boardWrapper">
+          <div className="board">
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
