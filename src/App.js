4984662692e919423d5483337433299b9b144e10
Qian <qian.ma3@dcmail.ca>
2020-02-26 22:15:48 -0500

    first round valid check

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, {Component} from 'react';
 import './App.css';
 // import logo from './logo.svg'
 
+
+let distanceX;
+let distanceY;
+let theOriginalX;
+let theOriginalY;
+let theMoveToX;
+let theMoveToY;
+let occupiedSpaces=[];
+let validPath = [];
+
+
 class App extends Component {
 
   constructor (props){
     super(props);
     this.state = {
       spaces:[],
-      chesses:[]
+      chesses:[],
     }
   }
 
@@ -18,9 +29,13 @@ class App extends Component {
     for (let y=1; y<9; y++) {
       for (let x=1; x<9; x++){
         spaces.push({
+          className: `space`,
           key: `space-${x}-${y}`,
+          location: `(${x},${y})`,
           locationX: x,
           locationY: y,
+          isOccupied: false,
+          isValidPath:false,
           style:{
             width:`9vh`,
             height:`9vh`,
@@ -43,9 +58,12 @@ class App extends Component {
         chesses.push({
           key: `chess-${x}-${y}`,
           // the locationX and locationY needs to be dynamically changed
-          
+          isSelected:false,
           locationX: x,
           locationY: y,
+          currentX: x,
+          currentY: y,
+          validPath: [],
           style:{
             width:`9vh`,
             height:`9vh`,
@@ -56,7 +74,8 @@ class App extends Component {
             zIndex:2,
             top: `${(y-1)*10}vh`,
             left: `${(x-1)*10}vh`,
-            position:`absolute`
+            position:`absolute`,
+            transformOrigin: `${(x-1)*10}vh ${(x-1)*10}vh`
           }
         })
       }
@@ -67,9 +86,12 @@ class App extends Component {
         chesses.push({
           key: `chess-${x}-${y}`,
           // the locationX and locationY needs to be dynamically changed
-          
+          isSelected:false,
           locationX: x,
           locationY: y,
+          currentX: x,
+          currentY: y,
+          validPath: [],
           style:{
             width:`9vh`,
             height:`9vh`,
@@ -80,7 +102,9 @@ class App extends Component {
             zIndex:2,
             top: `${(y-1)*10}vh`,
             left: `${(x-1)*10}vh`,
-            position:`absolute`
+            position:`absolute`,
+            // transformOrigin: `(${locationX}-1)vh ${(locationY-1)*10}vh`
+            transformOrigin: `${(x-1)*10}vh ${(y-1)*10}vh`
           }
         })
       }
@@ -88,10 +112,169 @@ class App extends Component {
     return {chesses};
   }
 
-
   // when click on any space, file the following event
-  chessMoveTo = (locationX, locationY) => (e) => {
-    console.log(`${locationX}`, `${locationY}`);
+  chessMoveTo = (locationX, locationY, isValidPath) => (e) => {
+    
+
+      // if the space is valid, get the x and y distance and file the animation event
+      theMoveToX = locationX;
+      theMoveToY = locationY;
+      distanceX = (theMoveToX - theOriginalX)*10;
+      distanceY = (theMoveToY - theOriginalY)*10;
+      console.log(`${distanceX}`, `${distanceY}`);
+      const selectedChess = document.querySelector(`.selectedChess`);
+      if (selectedChess) {
+        // find out if the clicked space is valid
+        if (isValidPath === true) {
+          selectedChess.style.transform = "translate3d(" + distanceX + "vh, " + distanceY + "vh, 0)";
+          selectedChess.classList.remove('selectedChess');
+            // change the state of the selected Chess, so that the caculation of valid spaces points can be updated
+          this.setState({
+            chesses: this.state.chesses.map((chess)=>{
+              if(chess.isSelected === true){
+                chess.currentX = locationX;
+                chess.currentY = locationY;
+              }
+              return chess;
+            }) 
+          })
+  
+        // update the isValidPath state of spaces
+        this.setState({
+          spaces: this.state.spaces.map((space)=>{
+            space.isValidPath = false;
+            space.className = `space`;
+            return space;
+          })
+        });
+      }else {
+        alert('this is not a valid move!');
+      }
+    } else {
+      alert('Please select the chess first!');
+    }
+    
+
+  }
+
+  // when click on any chess, file the following event
+  selectChess = (key,locationX, locationY, currentX, currentY) => (e) => {
+
+    // indicate which chess is being selected
+    const selectedChess = document.querySelector(`.selectedChess`);
+    if (selectedChess) {
+      selectedChess.classList.remove('selectedChess');
+      // selectedChess.style.boxShadow = 'none';
+    }
+    // e.target.style.boxShadow = "0px 0px 10px 5px gray";
+    e.target.classList.add('selectedChess');
+
+    // store the current location into global variable
+    theOriginalX = locationX;
+    theOriginalY = locationY;
+    console.log(`${locationX} , ${locationY} , chess selected`);
+
+    // change the state of the chess to indicate this is the one that's being selected
+    this.setState({
+      chesses: this.state.chesses.map((chess)=>{
+        chess.isSelected = false;
+        if(chess.key === key){
+          chess.isSelected = true;
+        }
+        return chess;
+      })
+    });
+
+    // update the isValidPath state of spaces
+    this.setState({
+      spaces: this.state.spaces.map((space)=>{
+        space.isValidPath = false;
+        space.className = `space`;
+        return space;
+      })
+    });
+
+    // find out the valid destination spaces
+    this.validPathCheck(currentX, currentY);
+      // first check
+  }
+
+  validPathCheck = (x,y) => {
+      // round 1 check
+      let firstRoundCheck = [];
+      if(x-1>0) {
+        firstRoundCheck.push(`(${x-1},${y})`);
+        if(y+1<9){
+          firstRoundCheck.push(`(${x-1},${y+1})`);
+        }
+        if(y-1>0){
+          firstRoundCheck.push(`(${x-1},${y-1})`);
+        }
+      }
+
+      if(x+1<9) {
+        firstRoundCheck.push(`(${x+1},${y})`);
+        if(y+1<9){
+          firstRoundCheck.push(`(${x+1},${y+1})`);
+        }
+        if(y-1>0){
+          firstRoundCheck.push(`(${x+1},${y-1})`);
+        }
+      }
+
+      if(y-1>0) {
+        firstRoundCheck.push(`(${x},${y-1})`);
+      }
+
+      if(y+1<9) {
+        firstRoundCheck.push(`(${x},${y+1})`);
+      }
+
+      console.log(firstRoundCheck);
+
+      firstRoundCheck.map((item) => {
+          this.setState({
+            spaces: this.state.spaces.map((space)=>{
+                if(space.location === item){
+                  console.log(space);
+                  if(space.isOccupied === false) {
+                    validPath.push(space);
+                    space.isValidPath = true;
+                    space.className = `chess isValidPath`;
+                  }
+                }
+                return space
+            })
+          })
+      })
+
+      console.log(validPath);
+  }
+
+
+ // check if the space is occupied or not
+  checkIfOccupied = () => {
+    occupiedSpaces = [];
+    this.state.spaces.map((space)=> {
+        this.state.chesses.map((chess)=>{
+          if(space.locationX === chess.currentX && space.locationY === chess.currentY){
+            occupiedSpaces.push(space);
+          }
+        })
+    });
+
+    this.setState({
+      spaces: this.state.spaces.map((space) => {
+          if (occupiedSpaces.includes(space)) {
+            space.isOccupied = true;
+          } else {
+            space.isOccupied = false;
+          }
+          return space
+      })
+    })
+
+    return occupiedSpaces;
   }
 
   componentDidMount() {
@@ -99,16 +282,19 @@ class App extends Component {
       ...this.getSpaces(),
       ...this.getChesses(),
     });
+
+    // update the isOccupied state of spaces constantly
+    setInterval(this.checkIfOccupied, 500);
   }
 
   render() {
     let {spaces, chesses} = this.state;
     let positionSpaces = spaces.map((space,index)=> { 
-      let {key, style, locationX, locationY} = space;
+      let {key, style, locationX, locationY, className, isValidPath} = space;
       let reactDom = <div
           key={key}
-          className="space"
-          onClick={this.chessMoveTo(locationX,locationY)}
+          className={className}
+          onClick={this.chessMoveTo(locationX,locationY, isValidPath)}
           style={{...style
           }}
         >
@@ -117,11 +303,11 @@ class App extends Component {
     });
 
     let positionChesses = chesses.map((chess,index)=> { 
-      let {key, style, locationX, locationY} = chess;
+      let {key, style, locationX, locationY, currentX, currentY} = chess;
       let reactDom = <div
           key={key}
           className="chess"
-          // onClick={this.chessMoveTo(locationX,locationY)}
+          onClick={this.selectChess(key,locationX,locationY, currentX, currentY)}
           style={{...style
           }}
         >
